c4cba74e60c55556dcdd21313a0b31a19533662a
Said Magomedov <65087195+smaaago@users.noreply.github.com>
2025-06-05 22:05:05 +0000

    minor changes

--- smartlab-streamlit.py
+++ smartlab-streamlit.py
@@ -21,9 +21,7 @@ selected_key = st.selectbox('Тикеры', list(data.keys()))
 
 if selected_key:
     ticker_stats = pd.DataFrame.from_dict(data[selected_key], orient='index')
-    ticker_stats.columns = ticker_stats.loc['date']
-    if ticker_stats.columns[-2] == ticker_stats.columns[-1]:
-        ticker_stats.columns = [*ticker_stats.columns[:-1]] + [f'LTM ({ticker_stats.columns[-1]})']
-    ticker_stats.drop('date', inplace=True)
+    ticker_stats.columns = ticker_stats.loc['year']
+    ticker_stats.drop('year', inplace=True)
     st.write(f'Выбранный тикер: {selected_key}')
     st.dataframe(ticker_stats)
